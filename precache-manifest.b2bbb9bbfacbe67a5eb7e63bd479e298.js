3594f0a2c4d74827a7c666b52532d6459edd1808
Tyler G <guthriet1230@gmail.com>
2019-02-16 14:22:48 -0600

    Updates

---
+++ precache-manifest.b2bbb9bbfacbe67a5eb7e63bd479e298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "794047f1fffdfe5ffde3",
+    "url": "/React-Memory-Game/static/js/runtime~main.794047f1.js"
+  },
+  {
+    "revision": "89f8df9bfecf014f7a84",
+    "url": "/React-Memory-Game/static/js/main.89f8df9b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/React-Memory-Game/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "89f8df9bfecf014f7a84",
+    "url": "/React-Memory-Game/static/css/main.59d4e3d8.chunk.css"
+  },
+  {
+    "revision": "fa87c0207d6b5abc9a2f12a6769e20b9",
+    "url": "/React-Memory-Game/index.html"
+  }
+];
