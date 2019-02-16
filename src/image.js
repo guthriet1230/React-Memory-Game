ce99482d14462807577fa16f46d41a1c826d68e8
Tyler G <guthriet1230@gmail.com>
2019-02-16 13:07:08 -0600

    Updates

--- src/image.js
+++
@@ -1,8 +0,0 @@
-const images = [
-  { name: "Sansa", charImg: "images/Sansa.jpg", index: 1 },
-  { name: "Sansa", charImg: "images/Jon.jpg", index: 2 },
-  { name: "Sansa", charImg: "images/Arya.jpg", index: 3 },
-  { name: "Sansa", charImg: "images/Robb.jpg", index: 4 }
-];
-
-export default images;
